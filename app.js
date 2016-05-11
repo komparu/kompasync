b28d088d2f1fb0769bf1d7fac927348d0bf2d35a
Kosta <kest.bodnya@gmail.com>
2016-05-11 10:25:36 +0200

    Logger, lol

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 var koa = require('koa')();
 var router = require('koa-router');
 var mount = require('koa-mount');
-var winston = require('winston');
+var logger = require('koa-logger');
 var jsonBody = require('koa-json-body');
 var api = require('./api.js');
 
@@ -15,5 +15,6 @@ var apiv1 = new router()
   .post('/merge', jsonBody(), api.all, api.merge);
 
 koa
+  .use(logger())
   .use(mount('/v1', apiv1.middleware()))
   .listen(1338);
